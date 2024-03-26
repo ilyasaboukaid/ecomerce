9efb041528f9e604e1e3146e0d46eeebb16c10ca
ilyasaboukaid <aboukaidilyas2@gmail.com>
2024-03-26 14:25:37 +0100

    les buttons marche

--- views/layout/parent.php
+++ views/layout/parent.php
@@ -15,13 +15,13 @@
         <a href="#"><img src="/phpCrud/web/assets//phpCrud/web/assets/img/logo.png" class="logo" alt=""></a>
         <div>
             <ul id="navbar">
-                <li><a class="active" href="index.html">Home</a></li>
-                <li><a href="shop.html">Shop</a></li>
+                <li><a class="active" href="site">Home</a></li>
+                <li><a href="produit">Shop</a></li>
                 <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="contact">Contact</a></li>
                 <li id="lg-bag"><a href="cart.html"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
-                <li id="account"><a href="login.html"><i class="fa fa-user-circle" aria-hidden="true"></i></a></li>
+                <li id="account"><a href="login"><i class="fa fa-user-circle" aria-hidden="true"></i></a></li>
                 <a href="#" id="close"><i class="fa fa-times" aria-hidden="true"></i></a>
             </ul>
         </div>
